fc932c94aeb7f405331f4f4147be8f82b4223cb7
Abhinav Panchal <abhipanchal44@gmail.com>
2022-05-09 22:19:30 -0400

    starting

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 // prod.js - production keys are here
 
-const { sendGridKey } = require("./dev");
-
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
